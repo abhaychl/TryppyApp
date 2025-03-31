5d983a2a63fa6897b3c9d10e68dadc6d09cc758e
om <om.saini@chlsoftech.com>
2025-03-31 16:22:43 +0530

    homecomponent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, Renderer2 } from '@angular/core';
-import { Router, } from '@angular/router';
+import { Router, RouterLink, } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,22 +8,10 @@ import { Router, } from '@angular/router';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  //public routes = routes
-
+  
   constructor(
     private router: Router,
-    private renderer:Renderer2
+    
   ){}
-  navigation(){
-   
-    //this.router.navigate([routes.index])
-  }
-  ngOnInit(): void {
-   // this.renderer.addClass(document.body, 'bg-light-200');
-  }
-  ngOnDestroy(): void {
-  //  this.renderer.removeClass(document.body, 'bg-light-200');
-  }
-
-  bsValue=new Date();
+  
 }
